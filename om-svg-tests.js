08d08b1018d759cf7c05639a156da4d089415e76
Jonas Enlund <jonas.enlund@gmail.com>
2014-04-09 19:54:07 +0300

    Added generated js

---
+++ om-svg-tests.js
@@ -0,0 +1,5 @@
+goog.addDependency("base.js", ['goog'], []);
+goog.addDependency("../cljs/core.js", ['cljs.core'], ['goog.string', 'goog.object', 'goog.string.StringBuffer', 'goog.array']);
+goog.addDependency("../om/dom.js", ['om.dom'], ['cljs.core']);
+goog.addDependency("../om/core.js", ['om.core'], ['cljs.core', 'om.dom', 'goog.ui.IdGenerator']);
+goog.addDependency("../om_svg_tests/core.js", ['om_svg_tests.core'], ['cljs.core', 'om.dom', 'goog.events.EventType', 'om.core', 'goog.events']);
